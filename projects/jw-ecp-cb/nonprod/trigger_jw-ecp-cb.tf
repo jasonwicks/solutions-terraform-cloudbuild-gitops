85722498283104ec4488b04e55c982782c7b6b68
Jason Wicks <jason.wicks@anz.com>
2020-05-25 17:44:21 +1000

    Update filers/triggers

--- projects/jw-ecp-cb/nonprod/trigger_jw-ecp-cb.tf
+++ projects/jw-ecp-cb/nonprod/trigger_jw-ecp-cb.tf
@@ -1,7 +1,7 @@
 
-resource "google_cloudbuild_trigger" "trigger" {
+resource "google_cloudbuild_trigger" "trigger_jw-ecp-cb" {
   
-  name        = "${module.jw-ecp-cb.project_id}-${var.envdir}-plan-apply"
+  name        = "jw-ecp-cb-${var.envdir}-plan-apply"
   description = "Standard trigger to execute level 3 pipeline for project anz-team-app"
 
   project     = "${module.jw-ecp-cb.project_id}"  
@@ -13,8 +13,8 @@ resource "google_cloudbuild_trigger" "trigger" {
   ** Note: ** is a recursive version of * which matches all files and directories in the selected directory and its subdirectories.
   ** For example, the pattern src/* will match src/code.py, but will ignore src/sub/code.py whereas src/** will match both.
   */
-  included_files = ["projects/anz-team-app/**"]
-  ignored_files  = ["projects/anz-team-app/prod/**"]  # Ignore any changes to production folder to avoid kicking off pipeline when commits don't impact nonprod folder.
+  included_files = ["projects/jw-ecp-cb/**"]
+  ignored_files  = ["projects/jw-ecp-cb/prod/**"]  # Ignore any changes to production folder to avoid kicking off pipeline when commits don't impact nonprod folder.
    
   trigger_template {
     branch_name = ".*"                     # We can execute trigger for all code changes, and determine in cloudbuild.yaml file to either plan or plan and apply
@@ -24,7 +24,7 @@ resource "google_cloudbuild_trigger" "trigger" {
  
   # Below is an example of how to pass variables into the cloudbuild.yaml file to add more powerful logic.
   substitutions = {
-    _PROJECT                 = "project/anz-team-app"      # path to project directory
+    _PROJECT                 = "project/jw-ecp-cb"      # path to project directory
     _ENVDIR                  = "${var.envdir}"             # prod or nonprod
     _RUN_TERRAFORM_APPLY     = "1"                         #
     _TERRAFORM_VERSION       = "${var.terraform_version}"  # Selects version of terraform to run.  Currently 11.8
